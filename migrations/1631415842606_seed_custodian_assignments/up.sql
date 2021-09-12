7ffa0cf49283486e1e4fdca8cbc0439a93748f29
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-09-12 11:07:49 +0800

    Add seed for custodian assignments

---
+++ migrations/1631415842606_seed_custodian_assignments/up.sql
@@ -0,0 +1,8 @@
+INSERT INTO custodian_assignment (name, description) VALUES
+  ('COMMON_SERVICES', 'This are expense of liaison, accounting etc'),
+  ('NORTH_MANILA', 'This are expense of North Manila sales and marketing activities. Normally expense for the area'),
+  ('LUZON_SALES_AND_MARKETING', 'This are expense of Luzon sales and marketing activities. Normally expense for the area'),
+  ('VISAYAS_SALES_AND_MARKETING', 'This are expense of Visayas sales and marketing activities. Normally expense for the area'),
+  ('MINDANAO_SALES_AND_MARKETING', 'This are expense of Mindanao sales and marketing activities. Normally expense for the area'),
+  ('REGULATORY', 'Sales assigned to regulatory functions'),
+  ('COMPANY_EXPENSE', 'Sales of company as to the company property (purchase of fix assets, cost of goods)');
